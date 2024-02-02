c6024b7c28d9824d2eda9dc2c9029f874c0dcf1f
devsrijit <srijitghosh7331@gmail.com>
2024-02-03 01:07:49 +0530

    Bug fix: resolved a potential possibility of subimtting same post twice.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,7 +8,6 @@ import { ThemeProvider } from "@/components/theme-provider"
 import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Bottombar from "@/components/shared/Bottombar";
-import RightSidebar from "@/components/shared/RightSidebar";
 import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -45,7 +44,6 @@ export default function RootLayout({
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
             {/* @ts-ignore */}
-            <RightSidebar />
           </main>
 
           <Bottombar />
